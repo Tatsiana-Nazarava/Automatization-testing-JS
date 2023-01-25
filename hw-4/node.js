58ebf8a43030d295260b3be77fc1e8c430c5b657
tnazarova <t.nazarova.qa@gmail.com>
2023-01-25 13:01:01 +0300

    the folder hw-4 contains the completed work for lesson 4

---
+++ hw-4/node.js
@@ -0,0 +1,23 @@
+const fs = require('fs-extra');
+
+// fs.mkdirSync('FirstFolder');
+
+// fs.createFileSync('./FirstFolder/File.txt');
+
+// fs.mkdirSync('SecondFolder');
+
+// fs.moveSync('FirstFolder/File.txt', 'SecondFolder/File.txt');
+
+// fs.mkdirSync('ThirdFolder');
+
+// fs.copyFileSync('SecondFolder/File.txt', 'ThirdFolder/File.txt');
+
+// fs.unlinkSync('SecondFolder/File.txt');
+
+// fs.unlinkSync('ThirdFolder/File.txt');
+
+// fs.removeSync('FirstFolder');
+
+fs.removeSync('SecondFolder');
+
+fs.removeSync('ThirdFolder');
