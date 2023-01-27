301aead84bc31a0317d56d4f15447fe95127c76a
Tatsiana-Nazarava <122973792+Tatsiana-Nazarava@users.noreply.github.com>
2023-01-27 11:36:00 +0300

    Update node.js
    removed comment marks

--- hw-4/node.js
+++ hw-4/node.js
@@ -1,22 +1,22 @@
 const fs = require('fs-extra');
 
-// fs.mkdirSync('FirstFolder');
+fs.mkdirSync('FirstFolder');
 
-// fs.createFileSync('./FirstFolder/File.txt');
+fs.createFileSync('./FirstFolder/File.txt');
 
-// fs.mkdirSync('SecondFolder');
+fs.mkdirSync('SecondFolder');
 
-// fs.moveSync('FirstFolder/File.txt', 'SecondFolder/File.txt');
+fs.moveSync('FirstFolder/File.txt', 'SecondFolder/File.txt');
 
-// fs.mkdirSync('ThirdFolder');
+fs.mkdirSync('ThirdFolder');
 
-// fs.copyFileSync('SecondFolder/File.txt', 'ThirdFolder/File.txt');
+fs.copyFileSync('SecondFolder/File.txt', 'ThirdFolder/File.txt');
 
-// fs.unlinkSync('SecondFolder/File.txt');
+fs.unlinkSync('SecondFolder/File.txt');
 
-// fs.unlinkSync('ThirdFolder/File.txt');
+fs.unlinkSync('ThirdFolder/File.txt');
 
-// fs.removeSync('FirstFolder');
+fs.removeSync('FirstFolder');
 
 fs.removeSync('SecondFolder');
 
