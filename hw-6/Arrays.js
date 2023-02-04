fbec7bd0d808622ea1e8aeedf687e07848b0bbaa
Tatsiana-Nazarava <122973792+Tatsiana-Nazarava@users.noreply.github.com>
2023-02-04 21:17:55 +0300

    Update Arrays.js

--- hw-6/Arrays.js
+++ hw-6/Arrays.js
@@ -1,4 +1,3 @@
-1
 let numberString = '1, 2, 3, 4, 5, 6';
 if(numberString[0] + numberString[1] + numberString[2] === numberString[3] + numberString[4] + numberString[5]) {
 console.log('Yes')
@@ -7,14 +6,12 @@ console.log('No')
 }
 
 
-2
 for (let n = 1000, num = 5; n >= 50; num++) {
 n = n / 2;
 console.log(n)
 }
 
 
-3
 const arr = [12, 15, 20, 25, 59, 79];
 const getAverage = (arr) => {
 let sum = 0; 
@@ -25,22 +22,19 @@ sum += arr[i];
 console.log(getAverage(arr));   
 
 
-4
 const myArray = [1, 2, 3, 4, 5];
 console.log(myArray.splice(3, 0, 'a', 'b', 'c'))
-console.log(myArray)
+console.log(myArray);
 
 
-5
 let myArray1 = [1, 2, 3, 4, 5];
 myArray1.splice(1, 0, 'a', 'b');
 myArray1.splice(6, 0, 'c');
 myArray1.splice(8, 0, 'e');
-console.log(myArray1)
+console.log(myArray1);
 
 
-6
 const myArray2 = [3, 4, 1, 2, 7];
-console.log(myArray2.sort((a, b) => (a - b)))
-console.log(myArray2.sort((a, b) => (b - a)))
+console.log(myArray2.sort((a, b) => (a - b)));
+console.log(myArray2.sort((a, b) => (b - a)));
 
