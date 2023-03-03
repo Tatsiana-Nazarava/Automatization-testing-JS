a2baa23d7221a1c138bb2edfa12402115959b1d6
Tatsiana-Nazarava <122973792+Tatsiana-Nazarava@users.noreply.github.com>
2023-03-03 21:07:06 +0300

    Update interfaces.js

--- homework-12/build/interfaces.js
+++ homework-12/build/interfaces.js
@@ -46,6 +46,7 @@ const persons = [
     }
 ];
 console.log(persons);
+// 3
 class ObjectManipulator {
     constructor(obj) {
         this.obj = obj;
@@ -67,21 +68,6 @@ class ObjectManipulator {
 }
 exports.ObjectManipulator = ObjectManipulator;
 //4 
-/**
- * 2 arguments passed: returns a new array
- * which is a result of input being mapped using
- * the specified mapper.
- *
- * 1 argument passed: returns a function which accepts
- * an input and returns a new array which is a result
- * of input being mapped using original mapper.
- *
- * 0 arguments passed: returns itself.
- *
- * @param {Function} mapper
- * @param {Array} input
- * @return {Array | Function}
- */
 function map(mapper, input) {
     if (arguments.length === 0) {
         return map;
@@ -97,22 +83,6 @@ function map(mapper, input) {
     return input.map(mapper);
 }
 exports.map = map;
-/**
- * 2 arguments passed: returns a new array
- * which is a result of input being filtered using
- * the specified filter function.
- *
- * 1 argument passed: returns a function which accepts
- * an input and returns a new array which is a result
- * of input being filtered using original filter
- * function.
- *
- * 0 arguments passed: returns itself.
- *
- * @param {Function} filterer
- * @param {Array} input
- * @return {Array | Function}
- */
 function filter(filterer, input) {
     if (arguments.length === 0) {
         return filter;
@@ -128,18 +98,6 @@ function filter(filterer, input) {
     return input.filter(filterer);
 }
 exports.filter = filter;
-/**
- * 2 arguments passed: returns sum of a and b.
- *
- * 1 argument passed: returns a function which expects
- * b and returns sum of a and b.
- *
- * 0 arguments passed: returns itself.
- *
- * @param {Number} a
- * @param {Number} b
- * @return {Number | Function}
- */
 function add(a, b) {
     if (arguments.length === 0) {
         return add;
