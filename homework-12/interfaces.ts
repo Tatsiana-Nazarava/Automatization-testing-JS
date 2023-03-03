96e1204cd66149fd90037fc0f58bcb41d7cad24d
Tatsiana-Nazarava <122973792+Tatsiana-Nazarava@users.noreply.github.com>
2023-03-03 20:57:07 +0300

    Update interfaces.ts

--- homework-12/interfaces.ts
+++ homework-12/interfaces.ts
@@ -81,17 +81,17 @@ console.log(persons)
 interface ObjectManipulatorInterface {
 
     constructor: object,
-    
-    set(key: string, value: object): object,
 
-    get(key: string): Array<typeof key>, 
+    set(key: string | number, value: string | number | object): string[] | object,
 
-    delete(key: string): Object | Array<typeof key> | string,
+    get(key: string): string[],
 
-    getObject(): object,   
+    delete(key: string): Object | string[] | string,
+
+    getObject(): object,
 
 }
- 
+
 export class ObjectManipulator implements ObjectManipulatorInterface {
 
     constructor(protected obj) {}
@@ -117,22 +117,6 @@ export class ObjectManipulator implements ObjectManipulatorInterface {
 
 //4 
 
-/**
- * 2 arguments passed: returns a new array
- * which is a result of input being mapped using
- * the specified mapper.
- *
- * 1 argument passed: returns a function which accepts
- * an input and returns a new array which is a result
- * of input being mapped using original mapper.
- *
- * 0 arguments passed: returns itself.
- *
- * @param {Function} mapper
- * @param {Array} input
- * @return {Array | Function}
- */
-
 export function map(mapper: Function, input: any) {
     if (arguments.length === 0) {
         return map;
@@ -148,28 +132,13 @@ export function map(mapper: Function, input: any) {
     return input.map(mapper);
 }
 
-/**
- * 2 arguments passed: returns a new array
- * which is a result of input being filtered using
- * the specified filter function.
- *
- * 1 argument passed: returns a function which accepts
- * an input and returns a new array which is a result
- * of input being filtered using original filter
- * function.
- *
- * 0 arguments passed: returns itself.
- *
- * @param {Function} filterer
- * @param {Array} input
- * @return {Array | Function}
- */
- export function filter(filterer: Function, input: any): Function | Array<typeof filter> {
+
+ export function filter(filterer: Function, input: any): Function | number[]| string[] {
     if (arguments.length === 0) {
         return filter;
     }
     if (arguments.length === 1) {
-        return function subFunction(subInput: any): Array<typeof subInput> | Function {
+        return function subFunction(subInput: any): Function | number[]| string[] {
             if (arguments.length === 0) {
                 return subFunction;
             }
@@ -180,18 +149,6 @@ export function map(mapper: Function, input: any) {
 }
 
 
-/**
- * 2 arguments passed: returns sum of a and b.
- *
- * 1 argument passed: returns a function which expects
- * b and returns sum of a and b.
- *
- * 0 arguments passed: returns itself.
- *
- * @param {Number} a
- * @param {Number} b
- * @return {Number | Function}
- */
 export function add(a: number, b:number): Number | Function {
     if (arguments.length === 0) {
         return add;
