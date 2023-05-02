3b5c7eee23ac0b10f75594bd0606175d4a738677
tnazarova <t.nazarova.qa@gmail.com>
2023-05-02 11:19:39 +0300

    Added prettier in the api-test folder

--- finalProject/api-tests/setupJest.js
+++ finalProject/api-tests/setupJest.js
@@ -1,39 +1,39 @@
 expect.extend({
-    toBeValidSchema(response, schema) {
-        const schemaValidationResult = validator.validate(response.data, schema);
-        let errorMessage;
-        if (schemaValidationResult.errors.length) {
-            errorMessage = schemaValidationResult.errors.reduce(function (accumulator, currentValue) {
-                return `${accumulator} ${currentValue.stack}`;
-            }, '');
+  toBeValidSchema(response, schema) {
+    const schemaValidationResult = validator.validate(response.data, schema);
+    let errorMessage;
+    if (schemaValidationResult.errors.length) {
+      errorMessage = schemaValidationResult.errors.reduce(function (accumulator, currentValue) {
+        return `${accumulator} ${currentValue.stack}`;
+      }, '');
+    }
+    return schemaValidationResult.valid
+      ? {
+          pass: true,
+          message: () => `The schemes are matched`,
         }
-        return schemaValidationResult.valid
-            ? {
-                pass: true,
-                message: () => `The schemes are matched`,
-            }
-            : {
-                pass: false,
-                message: () => `JSON schema validation error: ${errorMessage}
+      : {
+          pass: false,
+          message: () => `JSON schema validation error: ${errorMessage}
       Url: "${response.config.url}"
       Params: "${JSON.stringify(response.config.params)}"\n`,
-            };
-    },
+        };
+  },
 });
 
 expect.extend({
-    toBeValidStatusCode(response, expected) {
-        const comparingResult = response.status === expected || false;
+  toBeValidStatusCode(response, expected) {
+    const comparingResult = response.status === expected || false;
 
-        return comparingResult
-            ? {
-                pass: true,
-            }
-            : {
-                pass: false,
-                message: () => `The actual status code of the HTTP request is "${response.status}" expected status code "${expected}" 
+    return comparingResult
+      ? {
+          pass: true,
+        }
+      : {
+          pass: false,
+          message: () => `The actual status code of the HTTP request is "${response.status}" expected status code "${expected}" 
       Url: "${response.config.url}"
       Params: "${JSON.stringify(response.config.params)}"\n`,
-            };
-    },
+        };
+  },
 });
